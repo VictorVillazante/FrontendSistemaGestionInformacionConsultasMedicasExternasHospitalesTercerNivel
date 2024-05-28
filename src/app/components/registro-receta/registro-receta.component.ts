c077fecabdf3a87e7bbd79d71866f0304252e13a
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-28 13:46:39 -0400

    [Add] regitro de receta

--- src/app/components/registro-receta/registro-receta.component.ts
+++ src/app/components/registro-receta/registro-receta.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HistoriasClinicasService } from 'src/app/services/historias-clinicas.service';
+import { PacientesService } from 'src/app/services/pacientes.service';
+import { RecetasService } from '../../services/recetas.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro-receta',
@@ -6,5 +11,133 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro-receta.component.css']
 })
 export class RegistroRecetaComponent {
+  id:any;
+  formularioReceta: FormGroup;
+  pacientes:any[]=[];
+  filteredSuggestionsPacientes:any[]=[];
+  historiasClinicas:any[]=[];
+  filteredSuggestionsHistoriasClinicas:any[]=[];
 
+  constructor(private pacientesService:PacientesService,private fb: FormBuilder,private historiasClinicasService:HistoriasClinicasService,private recetasService:RecetasService) { 
+    this.formularioReceta = this.fb.group({
+      nombreGenericoMedicamentoPreescrito: [''],
+      viaCuidadoEspecialesAdministracion: [''],
+      concentracionDosificacion: [''],
+      frecuenciaAdministracion24hrs: [''],
+      duracionTratamiento: [''],
+      fechaVencimiento: [''],
+      precaucionesEspeciales: [''],
+      indicacionesEspeciales: [''],
+      idPaciente:[''],
+      ciPaciente:[''],
+      idHistoriaClinica:[''],
+      diagnosticoPresuntivo:['']
+    });
+  }
+  ngOnInit(){
+    this.pacientesService.obtenerPacientes().subscribe((data)=>{
+      this.pacientes=data;
+    });
+  }
+  isAutocompletePacienteFocused:boolean=false;
+  onFocusPaciente() {
+    this.isAutocompletePacienteFocused = true;
+  }
+
+  onBlurPaciente() {
+    setTimeout(() => {
+      this.isAutocompletePacienteFocused = false;
+    }, 500);
+  }
+
+  filterSuggestionsPaciente(event: Event): void {
+    const inputText = (event.target as HTMLInputElement)?.value ?? '';
+    if (inputText) {
+      this.filteredSuggestionsPacientes = this.pacientes.filter(paciente =>
+        paciente.ci.toLowerCase().includes(inputText.toLowerCase())
+      );
+    } else {
+      this.filteredSuggestionsPacientes = [];
+    }
+  }
+
+  isAutocompleteHistoriaClinicaFocused:boolean=false;
+  onFocusHistoriaClinica() {
+    this.isAutocompleteHistoriaClinicaFocused = true;
+  }
+
+  onBlurHistoriaClinica() {
+    setTimeout(() => {
+      this.isAutocompleteHistoriaClinicaFocused = false;
+    }, 500);
+  }
+
+  filterSuggestionsHistoriaClinica(event: Event): void {
+    const inputText = (event.target as HTMLInputElement)?.value ?? '';
+    if (inputText) {
+      this.filteredSuggestionsHistoriasClinicas = this.historiasClinicas.filter(historiaClinica =>
+        historiaClinica.diagnosticoPresuntivo.toLowerCase().includes(inputText.toLowerCase())
+      );
+    } else {
+      this.filteredSuggestionsHistoriasClinicas = [];
+    }
+  }
+
+  onSubmit() {
+    let historiaClinica=this.formularioReceta.value;
+    console.log(historiaClinica);
+    Swal.fire({
+      title: 'Estas seguro de realizar la accion',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      heightAuto:false,
+      
+      scrollbarPadding:true,
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.historiasClinicasService.registrarHistoriaClinica(historiaClinica).subscribe((data:any)=>{
+          Swal.fire({
+            icon: 'success',
+            text:"Se realizo la accion correctamente",
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#28afb0',
+            heightAuto:false,
+            scrollbarPadding:true,
+          })
+        },
+        (error: any) => {
+          Swal.fire({
+            icon: 'error',
+            text:"Error al realizar la accion",
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#28afb0',
+            heightAuto:false,
+            scrollbarPadding:true,
+          })
+        });
+      }
+    });
+    
+  }
+
+
+
+  seleccionarPaciente(paciente: any) {
+    this.isAutocompletePacienteFocused = false;
+    this.formularioReceta.controls['idPaciente'].setValue(paciente.idPaciente);
+    this.formularioReceta.controls['ciPaciente'].setValue(paciente.ci);
+    this.obtenerHistoriasClinicasPaciente(paciente.idPaciente);
+  }
+  seleccionarHistoriaClinica(historiaClinica: any) {
+    this.isAutocompletePacienteFocused = false;
+    this.formularioReceta.controls['idHistoriaClinica'].setValue(historiaClinica.idHistoriaClinica);
+    this.formularioReceta.controls['diagnosticoPresuntivo'].setValue(historiaClinica.diagnosticoPresuntivo);
+  }
+  obtenerHistoriasClinicasPaciente(idPaciente: any) {
+    this.historiasClinicasService.obtenerHistoriasClinicasDePaciente(idPaciente).subscribe((data)=>{
+      this.historiasClinicas=data;
+    });
+  }
 }
