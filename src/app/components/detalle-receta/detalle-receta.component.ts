a20d94a7cf9eb5b290312d71104708aa224b1256
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-05-28 20:04:02 -0400

    [Fix] aumentando informacion en interfaz detalle receta

--- src/app/components/detalle-receta/detalle-receta.component.ts
+++ src/app/components/detalle-receta/detalle-receta.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecetasService } from 'src/app/services/recetas.service';
 
 @Component({
@@ -17,7 +17,13 @@ export class DetalleRecetaComponent {
       this.obtenerReceta(this.id);
     });
   }
-  constructor(private activatedRoute: ActivatedRoute,private fb:FormBuilder,private recetasService:RecetasService){
+  verDetalleHistoriaClinica(idHistoriaClinica: any) {
+    const url = this.router.serializeUrl(
+     this.router.createUrlTree(['/medico/modificar-historia-clinica', idHistoriaClinica])
+   );
+   window.open(url, '_blank');
+  }
+  constructor(private router:Router,private activatedRoute: ActivatedRoute,private fb:FormBuilder,private recetasService:RecetasService){
     this.formularioReceta = this.fb.group({
       nombreGenericoMedicamentoPreescrito:[''],
       viaCuidadoEspecialesAdministracion:[''],
@@ -37,8 +43,18 @@ export class DetalleRecetaComponent {
   obtenerReceta(id:any){
     this.recetasService.obtenerReceta(id).subscribe((data:any)=>{
       this.formularioReceta.patchValue({
-        cambiosPacienteResultadosTratamiento:data.cambiosPacienteResultadosTratamiento,
+        nombreGenericoMedicamentoPreescrito:data.nombreGenericoMedicamentoPreescrito,
+        viaCuidadoEspecialesAdministracion:data.viaCuidadoEspecialesAdministracion,
+        concentracionDosificacion:data.concentracionDosificacion,
+        frecuenciaAdministracion24hrs:data.frecuenciaAdministracion24hrs,
+        duracionTratamiento:data.duracionTratamiento,
+        fechaVencimiento:data.fechaVencimiento,
+        precaucionesEspeciales:data.precaucionesEspeciales,
+        indicacionesEspeciales:data.indicacionesEspeciales,
+        idMedico:data.idMedico,
         idHistoriaClinica:data.idHistoriaClinica,
+        createdAt:data.createdAt,
+        updatedAt:data.updatedAt
       });
     });
   }
