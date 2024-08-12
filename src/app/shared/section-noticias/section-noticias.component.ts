9ee68b80a2a57017ae9434284aeca0a4d90096dc
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-08-12 12:45:18 -0400

    [Add] seccion noticias

--- src/app/shared/section-noticias/section-noticias.component.ts
+++ src/app/shared/section-noticias/section-noticias.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Noticia } from 'src/app/models/Noticia';
+import { ScriptsService } from 'src/app/services/scripts.service';
+import { v4 as uuidv4 } from 'uuid';
+import { InformacionCentroMedicoService } from '../../services/informacion-centro-medico.service';
 
 @Component({
   selector: 'app-section-noticias',
   templateUrl: './section-noticias.component.html',
   styleUrls: ['./section-noticias.component.css']
 })
-export class SectionNoticiasComponent {
-
+export class SectionNoticiasComponent implements OnInit{
+  id: string = uuidv4().replace(/-/g, '');
+  listadoNoticias: Noticia[] = [];
+  constructor(private scriptsService: ScriptsService,private informacionCentroMedicoService:InformacionCentroMedicoService) {
+  }
+  ngOnInit(): void {
+    this.informacionCentroMedicoService.obtenerNoticias().subscribe(noticias=>{
+      this.listadoNoticias=noticias;
+    })
+  }
+  ngAfterViewInit(): void {
+    this.scriptsService.cargarCarousel(this.id);
+  }
 }
