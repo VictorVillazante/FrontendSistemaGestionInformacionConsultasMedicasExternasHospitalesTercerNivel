8340a9ba1155ee71713e728c13c068cfb3643421
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-08-16 15:40:45 -0400

    [Fix] arreglando pasos procedimiento responsive

--- src/app/components/usuario-no-logeado-procedimientos/usuario-no-logeado-procedimientos.component.ts
+++ src/app/components/usuario-no-logeado-procedimientos/usuario-no-logeado-procedimientos.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Especialidad } from 'src/app/models/Especialidad';
 import { PasoProcedimiento } from 'src/app/models/PasoProcedimiento';
+import { InformacionCentroMedicoService } from 'src/app/services/informacion-centro-medico.service';
 
 @Component({
   selector: 'app-usuario-no-logeado-procedimientos',
@@ -7,5 +10,31 @@ import { PasoProcedimiento } from 'src/app/models/PasoProcedimiento';
   styleUrls: ['./usuario-no-logeado-procedimientos.component.css']
 })
 export class UsuarioNoLogeadoProcedimientosComponent {
+  tipo:string="";
+  idEspecialidad:number=0;
+  titulo:string="Procedimiento ";
   @Input() listadoProcedimientoPasos:PasoProcedimiento[]=[];
+  constructor(private route:ActivatedRoute,private informacionCentroMedicoService:InformacionCentroMedicoService){}
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.tipo = params['tipo'];
+      console.log(this.tipo);
+      this.titulo+=this.tipo.replace(/-/g," ");
+      this.idEspecialidad = params['id'];
+      this.obtenerEspecialidad(this.idEspecialidad);
+    });
+  }
+  obtenerEspecialidad(idEspecialidad: number) {
+    this.informacionCentroMedicoService.obtenerEspecialidad(this.idEspecialidad).subscribe((e)=>{
+      this.cargarRequisitosPorTipo(this.tipo,e);
+    })
+  }
+  cargarRequisitosPorTipo(tipo: string,especialidad:Especialidad) {
+    switch(this.tipo){
+      case "ficha-medica-presencial":
+        this.listadoProcedimientoPasos=especialidad.procedimientoObtencionFichaMedicaPresencial;
+      break;
+    }
+  }
+  
 }
