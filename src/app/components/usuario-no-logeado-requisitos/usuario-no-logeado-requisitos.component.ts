ec824c192908a6b360df3acb37e2e0b0fea0834e
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-08-16 15:23:19 -0400

    [Fix] arreglando requisitos

--- src/app/components/usuario-no-logeado-requisitos/usuario-no-logeado-requisitos.component.ts
+++ src/app/components/usuario-no-logeado-requisitos/usuario-no-logeado-requisitos.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Requisito } from 'src/app/models/Requisito';
+import { InformacionCentroMedicoService } from '../../services/informacion-centro-medico.service';
+import { Especialidad } from 'src/app/models/Especialidad';
 
 @Component({
   selector: 'app-usuario-no-logeado-requisitos',
@@ -8,16 +10,35 @@ import { Requisito } from 'src/app/models/Requisito';
   styleUrls: ['./usuario-no-logeado-requisitos.component.css']
 })
 export class UsuarioNoLogeadoRequisitosComponent implements OnInit{
-  param1:string="";
-  param2:string="";
+  tipo:string="";
+  idEspecialidad:number=0;
+  titulo:string="Requisitos ";
   @Input() listaRequisitos:Requisito[]=[];
-  constructor(private route:ActivatedRoute){}
+  constructor(private route:ActivatedRoute,private informacionCentroMedicoService:InformacionCentroMedicoService){}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.param1 = params['tipo'];
-      this.param2 = params['id'];
+      this.tipo = params['tipo'];
+      console.log(this.tipo);
+      this.titulo+=this.tipo.replace(/-/g," ");
+      this.idEspecialidad = params['id'];
+      this.obtenerEspecialidad(this.idEspecialidad);
     });
+  }
+  obtenerEspecialidad(idEspecialidad: number) {
+    this.informacionCentroMedicoService.obtenerEspecialidad(this.idEspecialidad).subscribe((e)=>{
+      this.cargarRequisitosPorTipo(this.tipo,e);
+    })
+  }
+  cargarRequisitosPorTipo(tipo: string,especialidad:Especialidad) {
+    switch(this.tipo){
+      case "obtencion-ficha-presencial":
+        this.listaRequisitos=especialidad.requisitosSolicitudFichaMedica;
+      break;
+      case "atencion-consulta-externa":
+        this.listaRequisitos=especialidad.requisitosMinimosAtencionConsultaExterna;
+      break;
+    }
   }
   
 }
