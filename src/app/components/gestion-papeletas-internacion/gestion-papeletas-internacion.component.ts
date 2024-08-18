b44d33b4ddedc47d9f74b70bd9b56cfcae4569e6
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-08-18 14:36:53 -0400

    [Fix] refactor gestion papeletas internacion

--- src/app/components/gestion-papeletas-internacion/gestion-papeletas-internacion.component.ts
+++ src/app/components/gestion-papeletas-internacion/gestion-papeletas-internacion.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CertificadosMedicosService } from '../../services/certificados-medicos.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gestion-papeletas-internacion',
   templateUrl: './gestion-papeletas-internacion.component.html',
   styleUrls: ['./gestion-papeletas-internacion.component.css']
 })
-export class GestionPapeletasInternacionComponent {
-  listadoPapeletasInternacion:any[]=[];
+export class GestionPapeletasInternacionComponent implements OnInit {
+  listadoPapeletasInternacion: any[] = [];
+  constructor(private router:Router,private certificadosMedicosService: CertificadosMedicosService) { }
+  handlePapeletasInternacion($event: any) {
+    this.listadoPapeletasInternacion = $event;
+  }
+  ngOnInit(): void {
+    this.certificadosMedicosService.obtenerCertificadosMedicos().subscribe((pi) => {
+      this.listadoPapeletasInternacion = pi;
+    })
+  }
+  actualizarPapeletaInternacion(id:number){
+    this.router.navigate(["medico/modificar-papeleta-internacion",id]);
+  }
+  verDetallePapeletaInternacion(id:number){
+    this.router.navigate(["medico/modificar-papeleta-internacion",id]);
+  }
 }
