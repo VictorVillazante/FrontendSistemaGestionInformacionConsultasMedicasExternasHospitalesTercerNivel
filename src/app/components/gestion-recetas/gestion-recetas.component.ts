1058c01ba2bff434991f8bffa2e6334059893816
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-08-18 15:52:38 -0400

    [Fix] refactor gestion de recetas

--- src/app/components/gestion-recetas/gestion-recetas.component.ts
+++ src/app/components/gestion-recetas/gestion-recetas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RecetasService } from 'src/app/services/recetas.service';
 import Swal from 'sweetalert2';
@@ -8,27 +8,31 @@ import Swal from 'sweetalert2';
   templateUrl: './gestion-recetas.component.html',
   styleUrls: ['./gestion-recetas.component.css']
 })
-export class GestionRecetasComponent {
-
+export class GestionRecetasComponent implements OnInit {
   recetas: any[] = [];
-  constructor(private router:Router,private recetasService:RecetasService) {
+  constructor(private router: Router, private recetasService: RecetasService) {
+  }
+  ngOnInit(): void {
     this.obtenerRecetas();
-   }
-   obtenerRecetas(){
-    this.recetasService.obtenerRecetas().subscribe((data:any)=>{
-      this.recetas=data;
+  }
+  handleRecetas($event: any) {
+    this.recetas = $event;
+  }
+  obtenerRecetas() {
+    this.recetasService.obtenerRecetas().subscribe((data: any) => {
+      this.recetas = data;
     });
   }
-  verDetalles(id:any){
-    this.router.navigate(["/detalle-receta",id]);
+  verDetalleReceta(id: any) {
+    this.router.navigate(["/detalle-receta", id]);
   }
-  editarDato(id: any) {
-    this.router.navigate(["/medico/modificar-receta",id]);
+  editarReceta(id: any) {
+    this.router.navigate(["/medico/modificar-receta", id]);
   }
   verDetalleHistoriaClinica(idHistoriaClinica: any) {
     const url = this.router.serializeUrl(
-     this.router.createUrlTree(['/medico/modificar-historia-clinica', idHistoriaClinica])
-   );
-   window.open(url, '_blank');
-   }
+      this.router.createUrlTree(['/medico/modificar-historia-clinica', idHistoriaClinica])
+    );
+    window.open(url, '_blank');
+  }
 }
