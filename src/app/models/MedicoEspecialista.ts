e422f3348d19d582aa4b634ae0436d7c1ca503e0
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-08-20 22:54:45 -0400

    [Fix] arreglando descipcion medico

--- src/app/models/MedicoEspecialista.ts
+++ src/app/models/MedicoEspecialista.ts
@@ -1,7 +1,15 @@
+import { Especialidad } from "./Especialidad";
+
 export class MedicoEspecialista{
     id:number=0;
     imagen:string="";
     nombres:string="";
     apellidos:string="";
+    email:string="";
+    celular:string="";
+    aniosExperiencia:number=0;
+    especialidades:any[]=[];
+    idiomas:any[]=[];
+    turnosAtencionMedica:any[]=[];
     descripcion:string="";
 }
--- src/assets/data-dev/medicos.ts
+++ src/assets/data-dev/medicos.ts
@@ -5,70 +5,40 @@ export class MedicosDataDev{
             "imagen": "doctor-home.png",
             "nombres": "Juan",
             "apellidos": "Pérez",
-            "descripcion": "Especialista en cardiología con 10 años de experiencia en el tratamiento de enfermedades del corazón."
-        },
-        {
-            "id": 2,
-            "imagen": "doctor-home.png",
-            "nombres": "Ana",
-            "apellidos": "García",
-            "descripcion": "Especialista en neurología, conocida por su investigación en enfermedades neurodegenerativas como el Alzheimer."
-        },
-        {
-            "id": 3,
-            "imagen": "url-de-imagen-3.jpg",
-            "nombres": "Luis",
-            "apellidos": "Rodríguez",
-            "descripcion": "Especialista en dermatología, experto en el tratamiento de enfermedades de la piel y estética dermatológica."
-        },
-        {
-            "id": 4,
-            "imagen": "url-de-imagen-4.jpg",
-            "nombres": "María",
-            "apellidos": "Fernández",
-            "descripcion": "Especialista en ginecología y obstetricia, con un enfoque en la salud reproductiva y el cuidado prenatal."
-        },
-        {
-            "id": 5,
-            "imagen": "doctor-home.png",
-            "nombres": "Pedro",
-            "apellidos": "Gómez",
-            "descripcion": "Especialista en ortopedia, con experiencia en la rehabilitación de fracturas y lesiones musculoesqueléticas."
-        },
-        {
-            "id": 6,
-            "imagen": "doctor-home.png",
-            "nombres": "Laura",
-            "apellidos": "Mendoza",
-            "descripcion": "Especialista en endocrinología, enfocada en el tratamiento de trastornos hormonales y metabólicos."
-        },
-        {
-            "id": 7,
-            "imagen": "doctor-home.png",
-            "nombres": "Jorge",
-            "apellidos": "Martínez",
-            "descripcion": "Especialista en urología, con experiencia en el tratamiento de enfermedades del tracto urinario y reproductor masculino."
-        },
-        {
-            "id": 8,
-            "imagen": "doctor-home.png",
-            "nombres": "Isabel",
-            "apellidos": "Sánchez",
-            "descripcion": "Especialista en oftalmología, dedicada a la prevención y tratamiento de enfermedades de los ojos."
-        },
-        {
-            "id": 9,
-            "imagen": "doctor-home.png",
-            "nombres": "Carlos",
-            "apellidos": "Torres",
-            "descripcion": "Especialista en medicina interna, con un enfoque en la gestión de enfermedades crónicas y condiciones múltiples."
-        },
-        {
-            "id": 10,
-            "imagen": "doctor-home.png",
-            "nombres": "Sofia",
-            "apellidos": "Vargas",
-            "descripcion": "Especialista en pediatría, con experiencia en el cuidado integral de la salud infantil y adolescencia."
-        }
+            "email": "juan.perez@example.com",
+            "celular": "+1234567890",
+            "aniosExperiencia": 5,
+            "especialidades": [
+                {
+                    "idEspecialidad": 1,
+                    "nombre": "Cardiología"
+                },
+                {
+                    "idEspecialidad": 2,
+                    "nombre": "Neurología"
+                }
+            ],
+            "idiomas": [
+              {
+                "id":1,
+                "nombre": "Español",
+                "nivel": "Nativo"
+              },
+              {
+                "id":2,
+                "nombre": "Inglés",
+                "nivel": "Avanzado"
+              }
+            ],
+            "turnosAtencionMedica": [
+              {
+                "id":1,
+                "nombreEspecialidad":"Cardiología",
+                "horaInicio":"09:00",
+                "horaFin":"12:30"
+              }
+            ],
+            "descripcion": "Soy un médico con más de 5 años de experiencia en el campo de la medicina. Me especializo en cardiología y medicina interna, y tengo un sólido historial en el diagnóstico y tratamiento de enfermedades cardíacas y trastornos internos. Hablo español e inglés, lo que me permite atender a una amplia gama de pacientes y colaborar en entornos internacionales. Ofrezco atención médica tanto en la mañana como en la tarde, adaptándome a las necesidades de mis pacientes. Mi objetivo es brindar un cuidado integral y personalizado para mejorar la salud y el bienestar de quienes confían en mí."
+          }
     ];    
 }
