148e3d0a0460708c11954da2ad710127afcfab73
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-10-16 15:15:46 -0400

    [Add] vista detalle examen complementario

--- src/app/components/detalle-examen-complementario/detalle-examen-complementario.component.ts
+++ src/app/components/detalle-examen-complementario/detalle-examen-complementario.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ExamenesComplementariosService } from 'src/app/services/examenes-complementarios.service';
 
 @Component({
   selector: 'app-detalle-examen-complementario',
@@ -6,5 +9,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./detalle-examen-complementario.component.css']
 })
 export class DetalleExamenComplementarioComponent {
+  verDetalleHistoriaClinica(arg0: any) {
+  }
+  examenComplementarioForm: FormGroup;
+  id:any;
+  constructor(private activatedRoute: ActivatedRoute,private fb: FormBuilder,private examenesComplementariosService:ExamenesComplementariosService){
+    this.examenComplementarioForm = this.fb.group({
+      nombre: [''],
+      descripcion: [''],
+      resumenResultados: [''],
+      fechaResultado: [''],
+      fechaSolicitud: [''],
+      idPaciente:[''],
+      pacientePropietario:[''],
+      ciPaciente:[''],
+      idMedico:[''],
+      nombreMedico:[''],
+      idHistoriaClinica: [''],
+      diagnosticoPresuntivo:[''],
+      createdAt:[''],
+      updatedAt:['']
+    });
+  }
+  ngOnInit(){
+    this.activatedRoute.params.subscribe( params => {
+      this.id = params['id'];
+      this.obtenerDetalleExamenComplementario(this.id);
+    });
+  }
+  obtenerDetalleExamenComplementario(idExamen:any){
+    this.examenesComplementariosService.obtenerExamenesComplementario(idExamen).subscribe((data:any)=>{
+      this.examenComplementarioForm.setValue({
+        nombre:data.nombre,
+        descripcion:data.descripcion,
+        resumenResultados:data.resumenResultados,
+        fechaResultado:data.fechaResultado,
+        fechaSolicitud:data.fechaSolicitud,
+        idPaciente:data.idPaciente,
+        ciPaciente:data.ciPropietario,
+        pacientePropietario:data.pacientePropietario,
+        idMedico:data.idMedico,
+        nombreMedico:data.nombreMedico,
+        idHistoriaClinica:data.idHistoriaClinica,
+        diagnosticoPresuntivo:data.diagnosticoPresuntivo,
+        createdAt:data.createdAt,
+        updatedAt:data.updatedAt
+      });
+    });
+  }
 
 }
